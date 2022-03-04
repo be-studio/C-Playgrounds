9174c52cf6cd717a98049767fb7e81421614e3f5
Eric Lew <eric@its-be-studio.com>
2022-03-04 11:49:26 +0000

    1. Installed and ran Doxygen; 2. Updated .gitignore.

--- data-types/wraparound.c
+++ data-types/wraparound.c
@@ -1,7 +1,19 @@
+/**
+ * BE C PLAYGROUND
+ * WRAPAROUND
+ *
+ * @file wraparound.c
+ * @author Eric L.
+ * @brief Demonstrates the wraparound effect on exceeding integer limits.
+ * @copyright (C)2022, BE Studio
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
 
+/**
+ * @return int
+ */
 int main() {
   unsigned int u = UINT_MAX;
   printf("UINT_MAX\t%u\n", u);
---
+++ html/search/all_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['wraparound_2ec_0',['wraparound.c',['../wraparound_8c.html',1,'']]]
+];
